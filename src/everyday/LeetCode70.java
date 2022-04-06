07486006852221457851016eafe4d41fb6b98d02
RivalZx <751774203@qq.com>
2022-04-06 16:33:21 +0800

    70-爬楼梯

---
+++ src/everyday/LeetCode70.java
@@ -0,0 +1,24 @@
+package everyday;
+
+/**
+ * @program: Algorithm
+ * @description: 爬楼梯
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * @author: he-zx
+ * @create: 2022-04-05 16:52
+ **/
+public class LeetCode70 {
+	public int climbStairs(int n) {
+		if (n == 1) return 1;
+		if (n == 2) return 2;
+		int a = 1, b = 2, temp;
+		for (int i = 3; i <= n; i++) {
+			temp = a;
+			a = b;
+			b = temp + b;
+		}
+		return b;
+	}
+}
